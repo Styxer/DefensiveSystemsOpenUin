f6edd7528ba9b1d6094aaa97718f9cb112e1d0e0
Ofir Rosner <gennin@gmail.com>
2023-02-20 22:42:44 +0200

    utils

---
+++ ממן15/Maman15/Client/Client/Constants.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <array>
+
+//The client version as 
+constexpr uint8_t client_version = 3;
+
+//The length of a client ID.
+constexpr uint8_t client_id_length = 16;
+
+//Maximum length of the username.
+constexpr size_t max_username_length = 255;
+
+//Maximum length of a filename.
+constexpr size_t max_filename_length = 255;
+
+//Amount of retries to do for the CRC check.
+constexpr size_t crc_retry_count = 4;
+
+//Client configuration filename  
+constexpr char CLIENT_CONFIG_FILENAME[] = "me.info";
+
+//Transfer configuration filename 
+constexpr char TRANSFER_CONFIG_FILENAME[] = "transfer.info";
+
+//the client ID.
+using client_id = std::array<uint8_t, client_id_length>;
+
+//the username
+using username = std::array<uint8_t, max_username_length>;
+
+//the filename
+using filename = std::array<uint8_t, max_filename_length>;
